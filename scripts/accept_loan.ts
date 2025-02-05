cd7156be156e875bc1981a192ea7e75ef765cc79
Benjamin Larkey <spartinofarrows@gmail.com>
2025-02-05 14:59:09 -0600

    added scripts, and added $ex mapping

--- scripts/accept_loan.ts
+++ scripts/accept_loan.ts
@@ -5,13 +5,13 @@ import { DeployFunction, Deployer, Network } from "@alephium/cli";
 import { Settings } from "../alephium.config";
 import { loadDeployments } from "../artifacts/ts/deployments";
 import { getNetwork } from "./network";
-import { CreateLoan } from "../artifacts/ts";
+import { AcceptLoan, CreateLoan } from "../artifacts/ts";
 
 const dotenv = require('dotenv');
 dotenv.config()
 
 const nodeProvider = new NodeProvider('https://node.mainnet.alephium.org')                  // Mainnet
-const signer = new PrivateKeyWallet({ privateKey: String(process.env.key), nodeProvider })
+const signer = new PrivateKeyWallet({ privateKey: String(process.env.twokey), nodeProvider })
 
 const deployScript: DeployFunction<Settings> = async (
     deployer: Deployer,
@@ -19,18 +19,12 @@ const deployScript: DeployFunction<Settings> = async (
   ): Promise<void> => {
     const upgradeNetwork = getNetwork()
     
-    let tx = await CreateLoan.execute(signer, {
+    let tx = await AcceptLoan.execute(signer, {
       initialFields: {
           loanFactory: "e8b899d2238e845321762afb6046afe6898fd37cd4140b3176349006850a9800",
-          tokenRequested: ALPH_TOKEN_ID,
-          tokenAmount: ONE_ALPH * 1n,
+          contract: "df3103c32d4e6999975417e316bb95a8820b42c95affd8cbcc0684a9be6fca00",
           tokenOracle: true,
-          collateralToken: ALPH_TOKEN_ID,
-          collateralAmount: ONE_ALPH * 3n,  
-          collateralOracle: true,
-          interest: 200n,
-          duration: 86400000n,
-          canLiquidate: false
+          collateralOracle: true
       },
       attoAlphAmount: DUST_AMOUNT + (MINIMAL_CONTRACT_DEPOSIT * 2n),
       tokens: [{id: ALPH_TOKEN_ID, amount: ONE_ALPH * 3n}]
--- scripts/payback_loan.ts
+++ scripts/payback_loan.ts
@@ -5,7 +5,7 @@ import { DeployFunction, Deployer, Network } from "@alephium/cli";
 import { Settings } from "../alephium.config";
 import { loadDeployments } from "../artifacts/ts/deployments";
 import { getNetwork } from "./network";
-import { CreateLoan } from "../artifacts/ts";
+import { AcceptLoan, CreateLoan, PayLoan } from "../artifacts/ts";
 
 const dotenv = require('dotenv');
 dotenv.config()
@@ -19,18 +19,10 @@ const deployScript: DeployFunction<Settings> = async (
   ): Promise<void> => {
     const upgradeNetwork = getNetwork()
     
-    let tx = await CreateLoan.execute(signer, {
+    let tx = await PayLoan.execute(signer, {
       initialFields: {
           loanFactory: "e8b899d2238e845321762afb6046afe6898fd37cd4140b3176349006850a9800",
-          tokenRequested: ALPH_TOKEN_ID,
-          tokenAmount: ONE_ALPH * 1n,
-          tokenOracle: true,
-          collateralToken: ALPH_TOKEN_ID,
-          collateralAmount: ONE_ALPH * 3n,  
-          collateralOracle: true,
-          interest: 200n,
-          duration: 86400000n,
-          canLiquidate: false
+          contract: "df3103c32d4e6999975417e316bb95a8820b42c95affd8cbcc0684a9be6fca00"
       },
       attoAlphAmount: DUST_AMOUNT + (MINIMAL_CONTRACT_DEPOSIT * 2n),
       tokens: [{id: ALPH_TOKEN_ID, amount: ONE_ALPH * 3n}]
