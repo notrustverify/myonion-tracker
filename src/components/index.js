9b9cf5747af38245e6ba78b7219c8c61dd16ca8a
OverGames <30346630+OverGamesDev@users.noreply.github.com>
2025-03-01 00:30:02 +0100

    doc link correction + discord added

--- src/components/index.js
+++ src/components/index.js
@@ -66,7 +66,7 @@ export function Hero() {
               className="mt-8 flex justify-center"
             >
               <Link
-                href="https://docs.alpacafi.org"
+                href="https://alphpacas.gitbook.io/alphpacas/alpacafi/what-is-alpacafi"
                 className="px-8 py-3 rounded-xl font-semibold bg-gray-700/50 hover:bg-gray-700 text-white border border-gray-700/50 hover:border-violet-400/30 transition-all duration-300 transform hover:scale-[1.02] active:scale-[0.98]"
               >
                 Documentation
